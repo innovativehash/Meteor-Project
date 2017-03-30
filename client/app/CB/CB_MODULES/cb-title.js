579892e1f529bea8b8abe3ddbc5807c85d9b47b9
Yeffry <smartlive.work@gmail.com>
2017-03-30 09:36:50 -0700

    increase size of title edit box

--- client/app/CB/CB_MODULES/cb-title.js
+++ client/app/CB/CB_MODULES/cb-title.js
@@ -5,49 +5,50 @@
  * TITLE-EDIT
  *********************************************************/
  export function titleEditText( e, t, P ) {
-      
+
     //GET THE CURRENTLY SELECTED ELEMENT
     let cur = t.$( '#cb-current' ).val();
-    
+
     //PULL OUT IT'S ACTUAL TEXT
     let text = t.$( `#${cur}` ).text().trim()
       , txt;
-      
+
     //HIDE THE CURRENT ELEMENT
     t.$( `#${cur}` ).hide();
-    
+
     //let pos = $( `${currentItem}` ).position();
-    
+
     //CREATE A NEW ELEMENT, ATTACH IT TO THE CANVAS, INJECT IT WITH THE TEXT
-    t.$( '#fb-template' ).append( `<input  id="toolb-added-title" 
+    t.$( '#fb-template' ).append( `<input  id="toolb-added-title"
                                             type="text"
                                             style="font-size:18px;
+                                            width:95%;
                                             font-weight:bold;z-index:2;
                                             border-radius:5px;
                                             background-color:white;
                                             cursor:move;
                                             border:1px dashed !important;"
-                                            value="${text}">` 
-                              ).css({ 'color': 'grey', 
-                                      'position': `element(${cur})`, 
-                                      'right': 0, 'bottom': 0 
+                                            value="${text}">`
+                              ).css({ 'color': 'grey',
+                                      'position': `element(${cur})`,
+                                      'right': 0, 'bottom': 0
                                     });
-    
+
     t.$( '#toolb-added-title' ).effect( "highlight", {}, 2000 ).focus();
-    
-    
+
+
     t.$( '#toolb-added-title' ).blur(function(){
       let txt = t.$( '#toolb-added-title' ).val().trim();
       t.$( '#toolb-added-title' ).remove();
-  
-      t.$( `#${cur}` ).show();  
+
+      t.$( `#${cur}` ).show();
       t.$( `#${cur}` ).text( txt );
-    
+
       let idx = P.indexOf( `${cur}` )
         , pos = t.$( `#${cur}` ).offset();
-        
+
       P.removeAt( idx );
-      P.insert( idx, { 
+      P.insert( idx, {
                         page_no:  t.page.get(),
                         id:       `${cur}`,
                         type:     'title',
@@ -61,62 +62,62 @@
                         textDecoration:   t.$( `#${cur}` ).css('text-decoration'),
                         opacity:          t.$( `#${cur}` ).css('opacity')
                     });
-                    
+
       P.print();
     });
-//---------------------------------------------------------   
- }  
+//---------------------------------------------------------
+ }
 
- 
+
    /********************************************************
    * TITLE-DELETE
    *******************************************************/
   export function titleDelete( e, t, P, pp ) {
     e.preventDefault();
-    
+
     let cur = t.$( '#cb-current' ).val()
       , idx = P.indexOf(`${cur}`);
-    
+
     P.removeAt( idx );
-    
+
     pp.update( { _id: Session.get('my_id') },
           { $pull: { objects:{ id:{$eq: cur} } }});
-        
+
     t.$( `#${cur}` ).remove();
     t.$( '#cb-current' ).val('');
-    
-    t.$( '#cb-title-toolbar' ).hide(); 
-    
+
+    t.$( '#cb-title-toolbar' ).hide();
+
     P.print();
     pp.find({}).fetch();
 //----------------------------------------------------------
   }
- 
- 
-  
-  
+
+
+
+
 /**********************************************************
  * TITLE-ITALIC  ::(CLICK)::
  *********************************************************/
   export function titleItalic( e, t, P ) {
     e.preventDefault();
-    
+
     let cur = t.$( '#cb-current' ).val()
       , idx = P.indexOf(`${cur}`)
       , pos = t.$( `#${cur}` ).offset();
-    
+
     if ( $( `#${cur}` ).css('fontStyle') != 'italic' ) {
       t.$( `#${cur}` ).css('fontStyle', 'italic');
-      //P.update( { _id: id, "objects.page_no": pg }, 
+      //P.update( { _id: id, "objects.page_no": pg },
       //          {$set:{"objects.$.fontStyle": 'italic' }});
     } else {
       t.$( `#${cur}` ).css('fontStyle', 'normal');
-      //P.update( { _id: id, "objects.page_no": pg }, 
+      //P.update( { _id: id, "objects.page_no": pg },
       //          {$set:{ "objects.$.fontStyle": 'normal' }});
     }
-    
+
     P.removeAt( idx );
-    P.insert( idx, { 
+    P.insert( idx, {
                       page_no:  t.page.get(),
                       id:       `${cur}`,
                       type:     'title',
@@ -130,34 +131,34 @@
                       textDecoration:   t.$( `#${cur}` ).css('text-decoration'),
                       opacity:          t.$( `#${cur}` ).css('opacity')
                   });
-                  
+
     console.log( P.print() );
 //---------------------------------------------------------
   }
-  
-  
+
+
  /**********************************************************
  * TITLE-BOLD ::(CLICK)::
  *********************************************************/
   export function titleBold( e, t, P ) {
     e.preventDefault();
-    
+
     let cur = t.$( '#cb-current' ).val()
       , idx = P.indexOf(`${cur}`)
       , pos = t.$( `#${cur}` ).offset();
-    
+
     if ( $( `#${cur}` ).css('fontWeight') != 'bold' ) {
       t.$( `#${cur}` ).css('fontWeight', 'bold');
-      //P.update( { _id: id, "objects.page_no": pg }, 
+      //P.update( { _id: id, "objects.page_no": pg },
       //          {$set:{ "objects.$.fontWeidht": 'bold'}});
     } else {
       t.$( `#${cur}` ).css('fontWeight', 'normal' );
-      //P.update( { _id: id, "objects.page_no": pg }, 
+      //P.update( { _id: id, "objects.page_no": pg },
       //          {$set:{ "objects.$.fontWeight": '' }});
     }
-    
+
     P.removeAt( idx );
-    P.insert( idx, { 
+    P.insert( idx, {
                       page_no:  t.page.get(),
                       id:       `${cur}`,
                       type:     'title',
@@ -171,35 +172,35 @@
                       textDecoration:   t.$( `#${cur}` ).css('text-decoration'),
                       opacity:          t.$( `#${cur}` ).css('opacity')
                   });
-                  
+
     console.log( P.print() );
 //---------------------------------------------------------
   }
-  
- 
+
+
  /**********************************************************
  * TITLE-UNDERLINE  ::(CLICK)::
  *********************************************************/
   export function titleUnderline( e, t, P ) {
     e.preventDefault();
-    
+
     let cur = t.$( '#cb-current' ).val()
       , idx = P.indexOf(`${cur}`)
       , pos = t.$( `#${cur}` ).offset();
 
-    
+
     if ( t.$( `#${cur}` ).css( 'textDecoration' ) != 'underline' ) {
       t.$( `#${cur}` ).css( 'textDecoration', 'underline' );
-      //P.update( { _id: id, "objects.page_no": pg }, 
+      //P.update( { _id: id, "objects.page_no": pg },
       //          {$set:{ "objects.$.textDecoration": 'underline' }});
     } else {
       t.$( `#${cur}` ).css('textDecoration', '');
-      //P.update( { _id: id, "objects.page_no": pg }, 
+      //P.update( { _id: id, "objects.page_no": pg },
       //          {$set:{ "objects.$.textDecoration": ''}});
     }
-    
+
     P.removeAt( idx );
-    P.insert( idx, { 
+    P.insert( idx, {
                       page_no:  t.page.get(),
                       id:       `${cur}`,
                       type:     'title',
@@ -213,27 +214,27 @@
                       textDecoration:   t.$( `#${cur}` ).css('text-decoration'),
                       opacity:          t.$( `#${cur}` ).css('opacity')
                   });
-                  
+
     console.log( P.print() );
 //---------------------------------------------------------
   }
-  
 
- 
+
+
  /**********************************************************
  * TITLE-FONT-SIZE  ::(INPUT)::
  *********************************************************/
   export function titleFontSizeInput( e, t ) {
     e.preventDefault();
-    
+
     let cur = t.$( '#cb-current' ).val()
       , fsz = t.$(e.currentTarget).val()
       , pg  = t.$( `#${cur}` ).data('page')
       , pos = t.$( `#${cur}` ).offset();
-    
+
 
     t.$( `#${cur}` ).css( 'font-size',`${fsz}px` );
-    
+
     t.$( '#fnt' ).val( fsz );
   }
 //---------------------------------------------------------
@@ -249,9 +250,9 @@
     let cur = t.$( '#cb-current' ).val()
       , idx = P.indexOf(`${cur}`)
       , pos = t.$( `#${cur}` ).offset();
-    
+
     P.removeAt( idx );
-    P.insert( idx, { 
+    P.insert( idx, {
                       page_no:  t.page.get(),
                       id:       `${cur}`,
                       type:     'title',
@@ -265,29 +266,29 @@
                       textDecoration:   t.$( `#${cur}` ).css('text-decoration'),
                       opacity:          t.$( `#${cur}` ).css('opacity')
                   });
-                  
+
     console.log( P.print() );
     return;
   }
-//---------------------------------------------------------  
-  
-  
-  
+//---------------------------------------------------------
+
+
+
 /**********************************************************
  * TITLE-OPACITY  ::(INPUT)::
  *********************************************************/
   export function titleOpacityInput( e, t ) {
     e.preventDefault();
-    
+
     let cur = t.$( '#cb-current' ).val()
       , id  = t.$( `#${cur}` ).data('pid')
       , top = t.$(e.currentTarget).val()
       , pg  = t.$( `#${cur}` ).data('page');
-    
+
     t.$( `#${cur}` ).css( 'opacity', top );
-    
+
     t.$( '#top' ).val( top );
-    //P.update( { _id: id, "objects.page_no":pg }, 
+    //P.update( { _id: id, "objects.page_no":pg },
     //          {$set:{"objects.$.opacity": op }});
   }
 //---------------------------------------------------------
@@ -302,9 +303,9 @@
     let cur = t.$( '#cb-current' ).val()
       , idx = P.indexOf(`${cur}`)
       , pos = t.$( `#${cur}` ).offset();
-    
+
     P.removeAt( idx );
-    P.insert( idx, { 
+    P.insert( idx, {
                       page_no:  t.page.get(),
                       id:       `${cur}`,
                       type:     'title',
@@ -318,8 +319,8 @@
                       textDecoration:   t.$( `#${cur}` ).css('text-decoration'),
                       opacity:          t.$( `#${cur}` ).css('opacity')
                   });
-                  
+
     console.log( P.print() );
     return;
   }
- //--------------------------------------------------------- 
+ //---------------------------------------------------------
