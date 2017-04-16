aa38055c46e2a39b945fe270d52547b7f085f44c
Yeffry <smartlive.work@gmail.com>
2017-04-16 20:38:17 +0600

    scripts added so that drag and drop works in mobile devices

--- client/app/ADMIN/certs.js
+++ client/app/ADMIN/certs.js
@@ -415,7 +415,22 @@ Template.certs.events({
 });
 //-----------------------------------------------------------------------------
 
+function touchHandler(event) {
+    var touch = event.changedTouches[0];
 
+    var simulatedEvent = document.createEvent("MouseEvent");
+        simulatedEvent.initMouseEvent({
+        touchstart: "mousedown",
+        touchmove: "mousemove",
+        touchend: "mouseup"
+    }[event.type], true, true, window, 1,
+        touch.screenX, touch.screenY,
+        touch.clientX, touch.clientY, false,
+        false, false, false, 0, null);
+
+    touch.target.dispatchEvent(simulatedEvent);
+    event.preventDefault();
+}
 
 function initC( d, c ) {
   let len = c.length;
@@ -449,6 +464,13 @@ function initC( d, c ) {
                          .prop('selectionEnd', 0);
 
   }
+  
+    document.addEventListener("touchstart", touchHandler, true);
+    document.addEventListener("touchmove", touchHandler, true);
+    document.addEventListener("touchend", touchHandler, true);
+    document.addEventListener("touchcancel", touchHandler, true);
+  
+  
 }
 
 /*
